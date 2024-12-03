4666fe7fbd392bcba78eb8523d3adc015a28c280
Mohd Shaheer <mshaheer2003@gmail.com>
2024-12-03 17:29:17 +0530

    initial commit

---
+++ Backend/src/index.ts
@@ -0,0 +1,30 @@
+import { WebSocket,WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+interface User{
+    socket: WebSocket;
+    room:string;
+}
+
+let allSockets: User[] = [];
+
+wss.on("connection",(socket)=>{
+
+    socket.on("message",(message)=>{
+        const parsedMessage = JSON.parse(message.toString());
+        if(parsedMessage.type == "join" ){
+            allSockets.push({
+                socket,
+                room:parsedMessage.payload.roomId
+            })
+            console.log(parsedMessage);
+        }
+        if(parsedMessage.type == "chat"){
+            console.log("here");
+            const currentUserRoom = allSockets.find((x)=> x.socket === socket )?.room;
+            allSockets.forEach((s)=> s.room == currentUserRoom && s.socket.send(parsedMessage.payload.message));
+        }
+    })
+   
+});
